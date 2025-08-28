856ea15d692925cc2549c20fb0786ba6824221d3
bart-bialk <bartbialk@gmail.com>
2025-08-28 12:21:17 +0200

    logout added

--- Application/views.py
+++ Application/views.py
@@ -19,8 +19,7 @@ def home(request):
     else:
         return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
-
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You are now logged out')
+    return redirect('home')
